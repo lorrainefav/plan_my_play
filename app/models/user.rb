85dc251a7a6888968169deb160461e1ade142d60
Yann Gregoire <yann.gregoire@yahoo.fr>
2018-11-21 16:21:24 +0100

    seeds ok withsupervisor, 1 tournament, players, 20 matches, 40 convocations

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :club, presence: true
-  validates :licence, uniqueness: true, presence: true, length: { is: 8 }
+  validates :licence, uniqueness: true, presence: true, length: { is: 9 }
   validates :gender, presence: true, inclusion: { in: [ "men", "women" ] }
   validates :ranking, presence: true, inclusion: { in: [ "NC", "40", "30/5", "30/4", "30/3", "30/2", "30/1", "30",\
   "15/5", "15/4", "15/3", "15/2", "15/1", "15", "5/6", "4/6", "3/6", "2/6", "1/6", "0", "-2/6", "-4/6", "-15", "TOP100" ] }
