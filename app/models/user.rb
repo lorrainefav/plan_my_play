5fcb8631b77b8df0d243749d026efac6926c0170
Yann Gregoire <yann.gregoire@yahoo.fr>
2018-11-24 12:13:13 +0100

    convocation associations modified

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :tournaments, foreign_key: :supervisor_id
   has_many :availabilities
-  has_many :convocations
   has_many :registrations
+  has_many :convocations, through: :registrations
 
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :licence, uniqueness: true, presence: true, length: { is: 9 }
-  validates :gender, presence: true, inclusion: { in: [ "men", "women" ] }
-  validates :ranking, presence: true, inclusion: { in: [ "NC", "40", "30/5", "30/4", "30/3", "30/2", "30/1", "30",\
+  validates :gender, inclusion: { in: [ "men", "women" ] }
+  validates :ranking, inclusion: { in: [ "NC", "40", "30/5", "30/4", "30/3", "30/2", "30/1", "30",\
   "15/5", "15/4", "15/3", "15/2", "15/1", "15", "5/6", "4/6", "3/6", "2/6", "1/6", "0", "-2/6", "-4/6", "-15", "TOP100" ] }
-  validates :phone_number, presence: true
 
   def to_s
     "#{first_name} #{last_name} - #{ranking} - #{club}"
