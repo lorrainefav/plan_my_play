d1b28d6e14b4f791f936bf68961c1f95aca8a4b1
gauthiervalin <gauthiervalin@hotmail.com>
2018-11-26 18:37:42 +0100

    Before merging crap

--- app/javascript/packs/datepicker.js
+++ app/javascript/packs/datepicker.js
@@ -2,13 +2,15 @@ import 'flatpickr/dist/flatpickr.min.css';
 import flatpickr from "flatpickr";
 
 const startDateElement = document.getElementById('start_date');
-flatpickr(startDateElement, {
-    enableTime: true,
-    altInput: true,
-    dateFormat: "Y-m-d H:i"});
-
 const endDateElement = document.getElementById('end_date');
-flatpickr(endDateElement, {
-    enableTime: true,
+
+flatpickr(startDateElement, {
+    enableTime: false,
     altInput: true,
-    dateFormat: "Y-m-d H:i"});
+    dateFormat: "Y-m-d"});
+
+
+flatpickr(endDateElement, {
+    enableTime: false,
+    altInput: true,
+    dateFormat: "Y-m-d"});
