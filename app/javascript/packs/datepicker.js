9daa85f1e21b1d581cd6330f9402f19cd5d2b32e
jules carriere <julescarriere2000@gmail.com>
2018-11-27 17:10:02 +0100

    implement date piccker

--- app/javascript/packs/datepicker.js
+++ app/javascript/packs/datepicker.js
@@ -4,14 +4,30 @@ import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
 
 
-const startDateElement = document.getElementById('start_date');
-const endDateElement = document.getElementById('end_date');
+// const startDateElement = document.getElementById('start_date');
+// const endDateElement = document.getElementById('end_date');
 
-flatpickr(startDateElement, {
-  minDate: "today",
-  altInput: true,
-  enableTime: true,
-  altFormat: "F j, Y",
-  dateFormat: "Y-m-d H:i",
-  "plugins": [new rangePlugin({ input: endDateElement})]
-});
+// flatpickr(startDateElement, {
+//   minDate: "today",
+//   altInput: true,
+//   enableTime: true,
+//   altFormat: "F j, Y",
+//   dateFormat: "Y-m-d H:i",
+//   "plugins": [new rangePlugin({ input: endDateElement})]
+// });
+
+
+const datepickers = document.querySelectorAll(".datepicker")
+
+datepickers.forEach((date_input) => {
+  const startDateElement = document.getElementById('start_date');
+  const endDateElement = document.getElementById('end_date');
+  flatpickr(startDateElement, {
+    minDate: "today",
+    altInput: true,
+    enableTime: true,
+    altFormat: "F j, Y",
+    dateFormat: "Y-m-d H:i",
+    "plugins": [new rangePlugin({ input: endDateElement})]
+  });
+})
