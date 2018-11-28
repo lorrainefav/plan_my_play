34e2181e678938df2189e4d0473f98f96866a901
Yann Gregoire <yann.gregoire@yahoo.fr>
2018-11-29 00:11:27 +0100

    grid ready to start filling it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_142052) do
+ActiveRecord::Schema.define(version: 2018_11_28_225822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2018_11_26_142052) do
     t.datetime "begin_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "court"
     t.index ["tournament_id"], name: "index_matches_on_tournament_id"
   end
 
