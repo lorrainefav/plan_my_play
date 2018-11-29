927cb2a5040dace5d306f64b39e02618b94dc24b
Yann Gregoire <yann.gregoire@yahoo.fr>
2018-11-29 15:56:54 +0100

    player_convocation modified

--- app/javascript/components/player_convocation_card.js
+++
@@ -1,7 +0,0 @@
-const refuseButton = document.getElementById('refuse-button');
-const acceptButton = document.getElementById('accept-button');
-const actionsButton = document.getElementById('action-buttons');
-
-acceptButton.addEventListener("click", () => {
-  console.log(acceptButton.value);
-})
--- app/javascript/packs/datepicker.js
+++ app/javascript/packs/datepicker.js
@@ -3,25 +3,27 @@ import 'flatpickr/dist/flatpickr.min.css';
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
 const datepickers = document.querySelectorAll(".datepicker")
+console.log(datepickers.length)
+if (datepickers.length !== 0) {
+  datepickers.forEach((date_inputs) => {
+    const startDateElement = date_inputs.querySelector('#start_date');
+    const endDateElement = date_inputs.querySelector('#end_date');
+    flatpickr(startDateElement, {
+      minDate: "today",
+      altInput: true,
+      enableTime: true,
+      altFormat: "F j, Y",
+      dateFormat: "Y-m-d H:i",
+      "plugins": [new rangePlugin({ input: endDateElement})]
+    });
+  })
 
-datepickers.forEach((date_inputs) => {
-  const startDateElement = date_inputs.querySelector('#start_date');
-  const endDateElement = date_inputs.querySelector('#end_date');
-  flatpickr(startDateElement, {
-    minDate: "today",
-    altInput: true,
-    enableTime: true,
-    altFormat: "F j, Y",
-    dateFormat: "Y-m-d H:i",
-    "plugins": [new rangePlugin({ input: endDateElement})]
-  });
-})
-
-const matchDateElement = document.querySelector('#match-time');
-flatpickr(matchDateElement, {
-    minDate: "today",
-    altInput: true,
-    enableTime: true,
-    altFormat: "F j, Y",
-    dateFormat: "Y-m-d H:i",
-  });
+  const matchDateElement = document.querySelector('#match-time');
+  flatpickr(matchDateElement, {
+      minDate: "today",
+      altInput: true,
+      enableTime: true,
+      altFormat: "F j, Y",
+      dateFormat: "Y-m-d H:i",
+    });
+}
