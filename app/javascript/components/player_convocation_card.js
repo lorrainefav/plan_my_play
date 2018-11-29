06e8577c9a66249bc1123222740163e3618a38f5
Yann Gregoire <yann.gregoire@yahoo.fr>
2018-11-29 13:26:28 +0100

    AJAX convocation en cours

---
+++ app/javascript/components/player_convocation_card.js
@@ -0,0 +1,7 @@
+const refuseButton = document.getElementById('refuse-button');
+const acceptButton = document.getElementById('accept-button');
+const actionsButton = document.getElementById('action-buttons');
+
+acceptButton.addEventListener("click", () => {
+  console.log(acceptButton.value);
+})
