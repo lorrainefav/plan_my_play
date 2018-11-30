567f8c067bbc5d303bb6f95308b22e19083bdaeb
Yann Gregoire <yann.gregoire@yahoo.fr>
2018-11-30 03:14:39 +0100

    correction player ok

--- app/controllers/convocations_controller.rb
+++ app/controllers/convocations_controller.rb
@@ -17,7 +17,7 @@ class ConvocationsController < ApplicationController
     @tournament = Tournament.find(params[:id])
     @tournament.send_convocations
     flash[:notice] = "Convocations envoyées !"
-    redirect_to registrations_path(@tournament)
+    redirect_to tournaments_path(@tournament)
   end
 
 private
